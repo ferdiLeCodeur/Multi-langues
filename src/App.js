75229065463eeb6f724ef93c2ef4dbff294b186b
FerdiLeCodeur <ferdi_Codeur@protonmail.com>
2022-03-09 09:43:35 +0100

    appli terminé

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Contenu from "./Components/Contenu/Contenu";
+import ToggleLangs from "./Components/ToggleLangs/ToggleLangs";
+import ContextProvider from "./context/langContext";
+
 
 function App() {
   return (
-    <>
+    <ContextProvider>
 
-    </>
+      <ToggleLangs></ToggleLangs>
+      <Contenu></Contenu>
+
+    </ContextProvider>
   );
 }
 
